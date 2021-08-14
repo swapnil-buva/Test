5bea8a2834b71691111b516a30a486da38b1bd4c
User <user.push@hackerrank.workspace>
2021-08-14 06:48:38 +0000

    Auto save changes before Run Tests
    Commit by HackerRank

--- src/main/java/com/hackerrank/selenium/DataExtractor.java
+++ src/main/java/com/hackerrank/selenium/DataExtractor.java
@@ -1,16 +1,23 @@
 package com.hackerrank.selenium;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.Map;
 
 public class DataExtractor {
 
-    public static Map<String, Integer> findTotalCasesByContinent(WebDriver driver, String pageUrl) {
-        return null;
+    public static Map<String, Integer> findTotalCasesByContinent(final WebDriver driver, final String pageUrl) {
+      ArrayList<WebElement> continent = new ArrayList<>(driver.findElements(By.xpath("//tr/td[5]")));  
+      System.out.println(continent.size());
+      Map<String, Integer> count = new HashMap<String, Integer>();
+      return count;
     }
 
-    public static Map<String, Integer> findTotalCountryByContinent(WebDriver driver, String pageUrl) {
+    public static Map<String, Integer> findTotalCountryByContinent(final WebDriver driver, final String pageUrl) {
         return null;
     }
 }
